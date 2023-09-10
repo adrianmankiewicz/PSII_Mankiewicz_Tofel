ac3bf05ff4655ea616563c6ec2cf1bc3a61e9a8e
adrianmankiewicz <adrianmankiewicz@onet.pl>
2023-09-10 19:00:22 +0200

    add

---
+++ PS2_Mankiewicz_Tofel/Intermediate/Source/PS2_Mankiewicz_Tofel.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class PS2_Mankiewicz_Tofel : ModuleRules
+{
+	public PS2_Mankiewicz_Tofel(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
---
+++ PS2_Mankiewicz_Tofel/Intermediate/Source/PS2_Mankiewicz_Tofel.cpp
@@ -0,0 +1,4 @@
+#include "CoreTypes.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultModuleImpl, PS2_Mankiewicz_Tofel, "PS2_Mankiewicz_Tofel");
